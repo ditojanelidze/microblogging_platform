a486de91f470b590a44743a29f2dea4e0cae4805
ditojanelidze <ditojanelidze28@gmail.com>
2020-12-19 17:36:34 +0400

    Add ip address validation to like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,15 @@
+require 'ipaddr'
+
 class Like < ApplicationRecord
   validates :ip_address, presence: true, length: {maximum: 15}
   validates_uniqueness_of :message_id, scope: :ip_address
+  validate :validate_ip_format
+
   belongs_to :message
+
+  def validate_ip_format
+    IPAddr.new(self.ip_address)
+  rescue => err
+    self.errors.add(:ip_address, :invalid, message: err.message)
+  end
 end
