6bfedac0559b784a80de48d5aa2c27cc2c78c366
ditojanelidze <ditojanelidze28@gmail.com>
2020-12-19 18:27:34 +0400

    Add swagger integration tests

--- spec/integration/messages_service_spec.rb
+++ spec/integration/messages_service_spec.rb
@@ -6,40 +6,43 @@ describe 'Messages', type: :request do
       tags 'Message'
       consumes 'application/json'
 
-      parameter name: :params,  in: :body, schema: {
+      parameter name: :params, in: :body, schema: {
         properties: {
-          author: {type: :string},
-          message: {type: :string}
+          author: { type: :string },
+          message: { type: :string }
         }
       }
 
       response '200', 'OK' do
         schema type: :object,
                properties: {
-                 status_code: {type: :integer},
+                 status_code: { type: :integer },
                  message: {
                    type: :object,
                    properties: {
-                     id: {type: :integer},
-                     author: {type: :string},
-                     message: {type: :string},
-                     created_at: {type: :string}
+                     id: { type: :integer },
+                     author: { type: :string },
+                     message: { type: :string },
+                     created_at: { type: :string }
                    }
                  }
                }
+        let(:params) { { author: 'Ruby', message: 'Hello World' } }
         run_test!
       end
 
       response '400', 'Bad request' do
         schema type: :object,
                properties: {
-                 status_code: {type: :integer},
-                 error_msg: {type: :string}
+                 status_code: { type: :integer },
+                 error_msg: { type: :string }
                }
+        let(:params) { { author: nil, message: nil } }
         run_test!
       end
     end
   end
+
   path '/message/{message_id}/like' do
     post 'Like Message' do
       tags 'Message'
@@ -50,25 +53,35 @@ describe 'Messages', type: :request do
       response '200', 'OK' do
         schema type: :object,
                properties: {
-                 status_code: {type: :integer},
+                 status_code: { type: :integer },
                  like: {
                    type: :object,
                    properties: {
-                     id: {type: :integer},
-                     ip_address: {type: :string},
-                     message_id: {type: :integer}
+                     id: { type: :integer },
+                     ip_address: { type: :string },
+                     message_id: { type: :integer }
                    }
                  }
                }
+        before do
+          @message = Message.create(author: "Ruby", message: "Hello World")
+        end
+
+        let(:message_id) { @message.id }
         run_test!
       end
 
       response '400', 'Bad request' do
         schema type: :object,
                properties: {
-                 status_code: {type: :integer},
-                 error_msg: {type: :string}
+                 status_code: { type: :integer },
+                 error_msg: { type: :string }
                }
+        before do
+          @message = Message.create(author: "Ruby", message: "Hello World")
+        end
+
+        let(:message_id) { @message.id + 1}
         run_test!
       end
     end
